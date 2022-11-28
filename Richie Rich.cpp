65b376efd239f6ba57e2bb1d4fa6f6f0c80dee35
Sahil Jawale <20510057.dypit@dypvp.edu.in>
2022-11-28 11:42:57 +0530

    Create Richie Rich.cpp

---
+++ Richie Rich.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define endl '\n'
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+
+void sahil(){
+    int a , b , c;
+    cin>>a>>b>>c;
+    
+    cout<<(b-a)/c<<endl;
+
+
+
+}
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin>>t;
+
+    while(t--){
+        sahil();
+    }
+    
+
+}
